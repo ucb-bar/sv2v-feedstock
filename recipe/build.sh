6a7d6f19064b978b76fd8bb17409530ce02e2718
abejgonzalez <abe.j.gonza@gmail.com>
2022-10-08 14:31:35 -0700

    Small speedups

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,23 +2,15 @@
 
 set -ex
 
-git submodule init
-git submodule update
-
-pushd llvm
-git fetch --unshallow
-popd
-
+rm -rf llvm/build
 mkdir llvm/build
 pushd llvm/build
 cmake -G Ninja ../llvm \
     -DLLVM_ENABLE_PROJECTS="mlir" \
     -DLLVM_TARGETS_TO_BUILD="host" \
-    -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DCMAKE_BUILD_TYPE=DEBUG \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-ninja
-ninja check-mlir
+    -DCMAKE_BUILD_TYPE=Release
+ninja -j${CPU_COUNT}
+ninja -j${CPU_COUNT} check-mlir
 popd
 
 mkdir build
@@ -27,11 +19,11 @@ cmake -G Ninja .. \
     -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
     -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH=$PREFIX
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 # TODO: unknown if needed
 #    -DLLVM_ENABLE_ASSERTIONS=ON \
-ninja
-ninja check-circt
-ninja check-circt-integration # Run the integration tests.
+ninja -j${CPU_COUNT}
+ninja -j${CPU_COUNT} check-circt
+ninja -j${CPU_COUNT} check-circt-integration # Run the integration tests.
 popd
