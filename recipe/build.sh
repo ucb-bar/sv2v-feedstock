dedd72a8b409b0d0a1e34e4df6c0884b69b1f578
abejgonzalez <abe.j.gonza@gmail.com>
2023-02-05 13:23:46 -0800

    Switch to sv2v

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,30 +2,6 @@
 
 set -ex
 
-rm -rf llvm/build
-mkdir llvm/build
-pushd llvm/build
-cmake -G Ninja ../llvm \
-    -DLLVM_ENABLE_PROJECTS="mlir" \
-    -DLLVM_TARGETS_TO_BUILD="host" \
-    -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-ninja -j${CPU_COUNT}
-ninja -j${CPU_COUNT} check-mlir
-popd
+make -j${CPU_COUNT}
 
-mkdir build
-pushd build
-cmake -G Ninja .. \
-    -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
-    -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
-    -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-ninja -j${CPU_COUNT}
-ninja -j${CPU_COUNT} check-circt
-ninja -j${CPU_COUNT} check-circt-integration # Run the integration tests.
-ninja install
-popd
+cp -f bin/* $PREFIX/bin/
